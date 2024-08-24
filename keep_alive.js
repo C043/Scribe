0ddd361cf1ad9b9e2412bc20d9b46d2ac74af97b
C043 <mariofragnitoph@gmail.com>
2024-08-24 18:41:25 +0200

    Adds keep alive code

--- keep_alive.js
+++ keep_alive.js
@@ -1,8 +1,13 @@
-let http = require("http");
+require("dotenv").config();
+const { env } = require("node:process");
+let express = require("express");
+const app = express();
+const port = env.PORT || 4000;
 
-http
-  .createServer(function (req, res) {
-    res.write("I'm alive");
-    res.end();
-  })
-  .listen(8000);
+app.createServer("/", (req, res) => {
+  res.send("I'm alive");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ const { Client, IntentsBitField } = require("discord.js");
 const DISCORD_TOKEN = env.DISCORD_TOKEN;
 const toUnicodeVariant = require("./toUnicodeVariant");
 
+// Keep alive
+let express = require("express");
+const app = express();
+const port = env.PORT || 4000;
+
+app.createServer("/", (req, res) => {
+  res.send("I'm alive");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
 const client = new Client({
   intents: [
     IntentsBitField.Flags.Guilds,
