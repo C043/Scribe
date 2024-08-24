f00a6a6b637cd4af95147844a8260c7c078aff90
C043 <mariofragnitoph@gmail.com>
2024-08-24 23:13:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,7 +42,7 @@ client.on("interactionCreate", async interaction => {
 
   if (interaction.commandName === "convert") {
     if (interaction.options.get("variant").value === "zalgo") {
-      interaction.reply(zalgoRandomGeneration(interaction.options.get("message").value, 2));
+      interaction.reply(zalgoRandomGeneration(interaction.options.get("message").value, 5));
     } else {
       interaction.reply(
         toUnicodeVariant(
